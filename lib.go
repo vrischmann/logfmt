1780d32efd55817776b358be211105ae11c8edc2
Vincent Rischmann <vincent@rischmann.fr>
2017-12-24 00:21:45 +0100

    initial commit

---
+++ lib.go
@@ -0,0 +1,160 @@
+package lgrep
+
+import (
+	"bytes"
+	"io"
+	"strconv"
+	"strings"
+)
+
+type Pair struct {
+	Key   string
+	Value string
+}
+
+type Pairs []Pair
+
+func Split(line string) Pairs {
+	parser := newPairParser()
+	parser.rd.Reset(line)
+
+	fn := parser.readKey
+
+	for {
+		nextFn := fn()
+		if nextFn == nil {
+			break
+		}
+
+		fn = nextFn
+	}
+
+	return parser.pairs
+}
+
+type pairParser struct {
+	rd  *strings.Reader
+	buf *bytes.Buffer
+
+	pairs       Pairs
+	currentPair Pair
+}
+
+func newPairParser() *pairParser {
+	return &pairParser{
+		rd:  new(strings.Reader),
+		buf: new(bytes.Buffer),
+	}
+}
+
+type stateFn func() stateFn
+
+func (p *pairParser) maybeMoveBufToValue(unquote bool) {
+	if p.buf.Len() > 0 {
+		p.currentPair.Value = p.buf.String()
+		if unquote {
+			p.currentPair.Value, _ = strconv.Unquote(p.currentPair.Value)
+		}
+		p.pairs = append(p.pairs, p.currentPair)
+	}
+}
+
+func (p *pairParser) readKey() stateFn {
+	p.consumeWhitespace()
+
+	p.currentPair.Key = ""
+	p.currentPair.Value = ""
+	p.buf.Reset()
+
+	for {
+		ch := p.readRune()
+		switch ch {
+		case eof:
+			return nil
+		case '=':
+			p.currentPair.Key = p.buf.String()
+			return p.readValue
+		default:
+			p.buf.WriteRune(ch)
+		}
+	}
+}
+
+func (p *pairParser) readValue() stateFn {
+	p.consumeWhitespace()
+
+	p.buf.Reset()
+
+	for {
+		ch := p.readRune()
+		switch ch {
+		case eof:
+			p.maybeMoveBufToValue(false)
+			return nil
+		case ' ':
+			p.maybeMoveBufToValue(false)
+			return p.readKey
+		case '"':
+			return p.readQuotedValue
+		default:
+			p.buf.WriteRune(ch)
+		}
+	}
+}
+
+// readQuotedValue reads a value that is double-quoted.
+// Leverages https://golang.org/pkg/strconv/#Unquote.
+func (p *pairParser) readQuotedValue() stateFn {
+	p.buf.Reset()
+
+	p.buf.WriteRune('"')
+
+	var ignoreNextQuote bool
+
+loop:
+	for {
+		ch := p.readRune()
+		switch ch {
+		case eof:
+			p.maybeMoveBufToValue(false)
+			return nil
+		case '\\':
+			p.buf.WriteRune(ch)
+			ignoreNextQuote = true
+		case '"':
+			p.buf.WriteRune(ch)
+			if ignoreNextQuote {
+				ignoreNextQuote = false
+				continue loop
+			}
+
+			p.maybeMoveBufToValue(true)
+
+			return p.readKey
+		default:
+			p.buf.WriteRune(ch)
+		}
+	}
+}
+
+var eof = rune(0)
+
+func (p *pairParser) readRune() rune {
+	ch, _, err := p.rd.ReadRune()
+	switch {
+	case err == io.EOF:
+		return eof
+	default:
+		return ch
+	}
+}
+
+func (p *pairParser) consumeWhitespace() {
+	for {
+		ch := p.readRune()
+		if ch != ' ' {
+			p.rd.UnreadRune()
+			return
+		}
+	}
+}
---
+++ lib_test.go
@@ -0,0 +1,47 @@
+package lgrep
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSplit(t *testing.T) {
+	testCases := []struct {
+		input string
+		exp   Pairs
+	}{
+		{
+			"ab=cd",
+			Pairs{
+				{"ab", "cd"},
+			},
+		},
+		{
+			"foo=bar 1=2    a=b",
+			Pairs{
+				{"foo", "bar"},
+				{"1", "2"},
+				{"a", "b"},
+			},
+		},
+		{
+			`str="foo bar baz" json="{\"Foo\":\"foo\",\"Bar\":\"bar\",\"Baz\":{\"A\":12,\"B\":4540,\"C\":{\"Opened\":true}}}"`,
+			Pairs{
+				{"str", "foo bar baz"},
+				{"json", `{"Foo":"foo","Bar":"bar","Baz":{"A":12,"B":4540,"C":{"Opened":true}}}`},
+			},
+		},
+		{
+			`json="\"{\\\"Foo\\\":\\\"foo\\\",\\\"Bar\\\":\\\"bar\\\",\\\"Baz\\\":{\\\"A\\\":12,\\\"B\\\":4540,\\\"C\\\":{\\\"Opened\\\":true}}}\""`,
+			Pairs{
+				{"json", `"{\"Foo\":\"foo\",\"Bar\":\"bar\",\"Baz\":{\"A\":12,\"B\":4540,\"C\":{\"Opened\":true}}}"`},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		res := Split(tc.input)
+		require.Equal(t, tc.exp, res)
+	}
+}
