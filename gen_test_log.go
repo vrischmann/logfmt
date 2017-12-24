ffbcfe2b80d2c4a8ca456f76beac8becc86f7445
Vincent Rischmann <vincent@rischmann.fr>
2017-12-24 14:38:06 +0100

    add a tool to generate test log files

---
+++ gen_test_log.go
@@ -0,0 +1,118 @@
+// +build ignore
+
+package main
+
+import (
+	"bufio"
+	"flag"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/vrischmann/logfmt"
+)
+
+func dictionaryWordGenerator() <-chan string {
+	f, err := os.Open("/usr/share/dict/words")
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	words := make([]string, 0, 65536)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		word := scanner.Text()
+		if strings.Contains(word, `'`) {
+			continue
+		}
+
+		word = strings.TrimSpace(word)
+		word = strings.ToLower(word)
+
+		words = append(words, word)
+	}
+
+	ch := make(chan string)
+
+	go func(words []string) {
+		for {
+			pos := rand.Intn(len(words))
+			ch <- words[pos]
+		}
+	}(words)
+
+	return ch
+}
+
+func makeOutputFile(filename string) *os.File {
+	if filename != "" {
+		f, err := os.Create(filename)
+		if err != nil {
+			logrus.Fatal(err)
+		}
+		return f
+	}
+
+	f, err := ioutil.TempFile("", "logfmt")
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	return f
+}
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+func main() {
+	var (
+		flSize   = flag.Int("size", 1024, "Size of the test file in megabytes")
+		flOutput = flag.String("output", "", "Output file")
+	)
+
+	flag.Parse()
+
+	maxSize := *flSize * 1024 * 1024
+
+	//
+
+	output := makeOutputFile(*flOutput)
+	defer output.Close()
+
+	logrus.Infof("output at %s", output.Name())
+
+	//
+
+	wordGenerator := dictionaryWordGenerator()
+
+	var (
+		buf          = make([]byte, 0, 4096)
+		totalWritten int
+	)
+	for totalWritten < maxSize {
+		nbPairs := rand.Intn(30)
+		if nbPairs < 4 {
+			nbPairs = 4
+		}
+
+		pairs := make(logfmt.Pairs, nbPairs)
+		for i := 0; i < nbPairs; i++ {
+			pairs[i].Key = <-wordGenerator
+			pairs[i].Value = <-wordGenerator
+		}
+
+		buf = pairs.AppendFormat(buf)
+		buf = append(buf, '\n')
+
+		n, err := output.Write(buf)
+		if err != nil {
+			logrus.Fatal(err)
+		}
+		totalWritten += n
+
+		buf = buf[:0]
+	}
+}
